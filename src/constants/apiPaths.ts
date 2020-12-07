d7f07496eab3e8b895b136c4c971dfff258ec650
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2020-12-07 22:42:56 +0300

    Serve cart API via Proxy API Gateway

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://g4hbmhzyx1.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://dltt76btqe.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://eugenealexeenko-cart-api-dev.eu-west-1.elasticbeanstalk.com/api'
+  cart: 'https://p3q59n9psc.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
